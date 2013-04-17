beaf3a94a7683c58269ed2ee1ff0e0baacf79747
jacoblwe20 <jacoblowe2.0@gmail.com>
2013-04-17 12:03:32 -0700

    updating slides

--- public/slides/js/css3-animation.js
+++ public/slides/js/css3-animation.js
@@ -20,4 +20,31 @@
 		}
 	});
 
+	// timing demo
+
+	var ele = document.getElementById("random");
+
+	setInterval(function(){
+	  var randX = Math.round(Math.random() * 100);
+	  var randY = Math.round(Math.random() * 100);
+	  ele.style.webkitTransform = "translate(" + randX + "px, " + randY + "px)";
+	},500);
+
+	var adv = document.getElementById("advancing");
+	var ogClass = adv.className;
+
+	adv.addEventListener("click", function(e){
+	  var classNames = e.target.className;
+
+	  if(!/first/g.test(classNames)){
+	  	adv.className = classNames + " first";
+	  }else if(!/second/g.test(classNames)){
+	  	adv.className = classNames + " second";
+	  }else if(!/third/g.test(classNames)){
+	  	adv.className = classNames + " third";
+	  }else{
+	  	adv.className = ogClass;
+	  }
+	})
+
 }())
